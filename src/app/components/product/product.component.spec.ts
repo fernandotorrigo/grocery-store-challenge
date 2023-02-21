f72daf3fa85ba2e3247701fed7feadbd1a7f8195
Fernando Torrigo <fernandotorrigo@Fernandos-MBP.home>
2023-02-21 17:09:58 -0300

    testing product component

--- src/app/components/product/product.component.spec.ts
+++ src/app/components/product/product.component.spec.ts
@@ -1,27 +1,137 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Store } from '@ngrx/store';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  ComponentFixture,
+  fakeAsync,
+  flush,
+  TestBed
+} from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { initialStateMock, stateDefaultMock } from 'src/app/shared/mocks';
+import { Cart, RootState } from 'src/app/shared/models';
 import { ProductComponent } from './product.component';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
-  let store: MockStore<{ loggedIn: boolean }>;
-  const initialState = { loggedIn: false };
+  let store: MockStore<RootState>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ProductComponent],
-      providers: [provideMockStore({ initialState })],
+      imports: [FormsModule, ReactiveFormsModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [provideMockStore({ initialState: initialStateMock })],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
-    store = TestBed.get<Store>(Store);
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Should be in cart the product with index 1', fakeAsync(() => {
+    const currentProduct = stateDefaultMock.groceryRootStore.products[1];
+    component.product = currentProduct;
+    fixture.detectChanges();
+
+    store.setState(stateDefaultMock);
+    store.refreshState();
+    flush();
+
+    expect(component.inCart).toBe(true);
+  }));
+
+  it('Should not be in cart the product with index 3', fakeAsync(() => {
+    const currentProduct = stateDefaultMock.groceryRootStore.products[3];
+    component.product = currentProduct;
+    fixture.detectChanges();
+
+    store.setState(stateDefaultMock);
+    store.refreshState();
+    flush();
+
+    expect(component.inCart).toBe(false);
+  }));
+
+  it('Should add in cart the product with index 3', fakeAsync(() => {
+    const currentProduct = stateDefaultMock.groceryRootStore.products[3];
+    component.product = currentProduct;
+    fixture.detectChanges();
+
+    component.addToCart(currentProduct);
+    store.refreshState();
+    flush();
+
+    expect(component.inCart).toBe(true);
+    expect(component.quantity).toBe(1);
+  }));
+
+  it('Should remove from cart the product with index 1', fakeAsync(() => {
+    const currentProductToRemove =
+      stateDefaultMock.groceryRootStore.products[0];
+
+    const newState = {
+      groceryRootStore: {
+        ...stateDefaultMock.groceryRootStore,
+        cart: stateDefaultMock.groceryRootStore.cart.filter(
+          (item) => item.id !== currentProductToRemove.id
+        ),
+      },
+    };
+
+    component.product = currentProductToRemove;
+    store.setState(newState);
+    store.refreshState();
+    flush();
+
+    component.removeFromCart(currentProductToRemove);
+
+    component.cart$.subscribe((cart) => {
+      const itemIsInCart = cart.filter(
+        (item) => item.id === currentProductToRemove.id
+      );
+      expect(itemIsInCart).toHaveSize(0);
+    });
+
+    expect(component.inCart).toBe(false);
+  }));
+
+  it('Should update the quantity in cart on the product with index 0', fakeAsync(() => {
+    const newQtd = 4;
+    const currentProduct = stateDefaultMock.groceryRootStore.products[0];
+    component.product = currentProduct;
+
+    const defaultCart: Cart[] = stateDefaultMock.groceryRootStore.cart;
+    const newCart = defaultCart.map((data) => {
+      return {
+        ...data,
+        quantity: currentProduct.id === data.id ? newQtd : data.quantity,
+      };
+    });
+
+    const currentState = {
+      groceryRootStore: {
+        ...stateDefaultMock.groceryRootStore,
+        cart: newCart,
+      },
+    };
+    component.updateQuantity(4, currentProduct);
+    store.setState(currentState);
+    store.refreshState();
+    flush();
+
+    component.cart$.subscribe((cart) => {
+      expect(
+        cart.filter((item) => item.id === currentProduct.id)[0].quantity
+      ).toEqual(4);
+    });
+
+    expect(component.inCart).toBe(true);
+    expect(component.quantity).toBe(4);
+  }));
 });
--- src/app/shared/mocks/index.ts
+++ src/app/shared/mocks/index.ts
@@ -9,3 +9,59 @@ export const initialStateMock: RootState = {
     cart: [],
   },
 };
+
+export const stateDefaultMock: RootState = {
+  groceryRootStore: {
+    products: [
+      {
+        id: 1,
+        name: 'Peas',
+        price: 0.95,
+        image: '/assets/images/peas.png',
+        description: 'Peas per bag',
+      },
+      {
+        id: 2,
+        name: 'Eggs',
+        price: 2.1,
+        image: '/assets/images/eggs.png',
+        description: 'Eggs per dozen',
+      },
+      {
+        id: 3,
+        name: 'Milk',
+        price: 1.3,
+        image: '/assets/images/milk.png',
+        description: 'Milk per bottle',
+      },
+      {
+        id: 4,
+        name: 'Beans',
+        price: 0.73,
+        image: '/assets/images/beans.png',
+        description: 'Baked Beans - per can',
+      },
+    ],
+    currencyData: {
+      BRL: 123,
+    },
+    cart: [
+      {
+        id: 1,
+        quantity: 1,
+        name: 'Peas',
+        price: 0.95,
+        image: '/assets/images/peas.png',
+        description: 'Peas per bag',
+      },
+      {
+        id: 2,
+        quantity: 1,
+        name: 'Eggs',
+        price: 2.1,
+        image: '/assets/images/eggs.png',
+        description: 'Eggs per dozen',
+      },
+    ],
+  },
+};
